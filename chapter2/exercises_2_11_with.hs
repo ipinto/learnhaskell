a7713e9fef00ffe093c905929f642f2d9f48cc31
Imanol Pinto <i.pinto@lookiero.com>
2020-07-01 20:19:30 +0200

    Add exercises done in chapter 2.11 (with)

---
+++ chapter2/exercises_2_11_with.hs
@@ -0,0 +1,11 @@
+module Exercises_2_11_where where
+
+-- Rewrite with 'where' the following expressions
+-- z = 7
+-- y = z + 8
+-- x = y ^ 2
+-- waxOn = x * 5
+waxOn = x * 5
+  where z = 7
+        y = z + 8
+        x = y ^ 2
