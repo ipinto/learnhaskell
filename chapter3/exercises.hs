dc1d8e2d416674362204ff9770f6e9fb6af2f6b2
Imanol Pinto <i.pinto@lookiero.com>
2020-07-05 19:17:41 +0200

    Add String exercises

---
+++ chapter3/exercises.hs
@@ -0,0 +1,29 @@
+module Exercises where
+
+-- "Hello world" --> "ello world"
+exercise1 :: String -> String
+exercise1 x = tail x
+
+exercise1Other :: [Char] -> String
+exercise1Other x = drop 1 x
+
+-- "Curry is awesome" --> "Curry is awesome!"
+exercise2 :: [Char] -> [Char]
+exercise2 x = x ++ "!"
+
+exercise2Other :: String -> [Char]
+exercise2Other x = (++) x "!"
+
+-- "Curry is awesome!" --> "y"
+exercise3 :: [Char] -> Char
+exercise3 x = x !! 4
+
+exercise3Other :: String -> Char
+exercise3Other x = head (drop 4 x)
+
+exercise3OtherMore :: [Char] -> [Char]
+exercise3OtherMore x = drop 4 (take 5 x)
+
+-- "Curry is awesome!" --> "awesome!"
+exercise4 :: String -> String
+exercise4 x = drop 9 x
