18dda266400c333ad5080a39ded5d442ca87fac8
Imanol Pinto <i.pinto@lookiero.com>
2020-10-21 18:24:26 +0200

    Add first exercise

---
+++ chapter09/enumFromTo.hs
@@ -0,0 +1,28 @@
+module EnumFromTo where
+
+-- Write your own enumFromTo definitions for the types provided.
+
+eftBool :: Bool -> Bool -> [Bool]
+eftBool True True = [True]
+eftBool True _ = []
+eftBool False False = [False]
+eftBool False _ = False : True : []
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd a b
+      | a > b = []
+      | a == b = [a]
+      | otherwise = a : eftOrd (succ a) b
+
+eftInt :: Int -> Int -> [Int]
+eftInt a b
+      | a > b = []
+      | a == b = [a]
+      | otherwise = a : eftInt (succ a) b
+
+eftChar :: Char -> Char -> [Char]
+eftChar a b
+      | a > b = []
+      | a == b = [a]
+      | otherwise = a : eftChar (succ a) b
+
